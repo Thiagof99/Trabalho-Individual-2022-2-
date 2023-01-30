7a8f25d846ec91c7dfac36e477db0d075b49ed8d
thiago <thiago3399@hotmail.com>
2023-01-29 23:30:31 -0300

    4.Documentação Automatizada

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc', 'breathe'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -59,7 +59,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -175,3 +175,6 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+breathe_projects = { "trabalho-final_ThiagoOliveira": "../doxygen/xml/index.xml" }
+breathe_default_project = "trabalho-final_ThiagoOliveira"
